1f1bc57e00883c8e77c37e919d554b11b3a3b559
carlos-bolano <carlostutos828@gmail.com>
2023-08-11 10:32:11 -0500

    chore: add the url of the deployed api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://api-task-manager-production.up.railway.app/api',
   withCredentials: true
 })
 
