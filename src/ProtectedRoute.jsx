47907b7bb3b45b666bebc69ce5e5ee595a714477
carlos-bolano <carlostutos828@gmail.com>
2024-05-04 11:07:13 -0500

    feat: add a Loader component

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,13 +1,12 @@
-import { Navigate, Outlet } from 'react-router-dom'
-import { useAuth } from './context/AuthContext'
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
+import Loader from "./components/Loader";
 
-function ProtectedRoute () {
-  const { isAutenticated, loading } = useAuth()
-  if (loading) return <h1 className='text-center text-primary text-3xl'>Loading...</h1>
-  if (!isAutenticated && !loading) return <Navigate to="/login" replace />
-  return (
-    <Outlet />
-  )
+function ProtectedRoute() {
+  const { isAutenticated, loading } = useAuth();
+  if (loading) return <Loader />;
+  if (!isAutenticated && !loading) return <Navigate to="/login" replace />;
+  return <Outlet />;
 }
 
-export default ProtectedRoute
+export default ProtectedRoute;
