0aa2638830e66ac6624a2acfa73aa6a3e3e99ba0
carlos-bolano <carlostutos828@gmail.com>
2024-05-04 11:06:33 -0500

    feat: put TaskPage as main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,38 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import RegisterPage from './pages/RegisterPage'
-import LoginPage from './pages/LoginPage'
-import { AuthProvider } from './context/AuthContext'
-import ProfilePage from './pages/ProfilePage'
-import TasksPage from './pages/TasksPage'
-import TasksFormPage from './pages/TasksFormPage'
-import HomePage from './pages/HomePage'
-import ProtectedRoute from './ProtectedRoute'
-import LayoutHome from './layouts/LayoutHome'
-import { TaskProvider } from './context/TasksContext'
-import SeeTaskPage from './pages/SeeTaskPage'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import { AuthProvider } from "./context/AuthContext";
+import ProfilePage from "./pages/ProfilePage";
+import TasksPage from "./pages/TasksPage";
+import TasksFormPage from "./pages/TasksFormPage";
+import ProtectedRoute from "./ProtectedRoute";
+import LayoutHome from "./layouts/LayoutHome";
+import { TaskProvider } from "./context/TasksContext";
+import SeeTaskPage from "./pages/SeeTaskPage";
 
-function App () {
+function App() {
   return (
     <AuthProvider>
       <TaskProvider>
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/register' element={<RegisterPage />} />
-            
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+
             <Route element={<ProtectedRoute />}>
               <Route element={<LayoutHome />}>
-                <Route path='/tasks' element={<TasksPage />} />
-                <Route path='/tasks/:id' element={<SeeTaskPage />} />
-                <Route path='/update-task/:id' element={<TasksFormPage />} />
-                <Route path='/add-task' element={<TasksFormPage />} />
-                <Route path='/profile' element={<ProfilePage />} />
+                <Route path="/" element={<TasksPage />} />
+                <Route path="/tasks/:id" element={<SeeTaskPage />} />
+                <Route path="/update-task/:id" element={<TasksFormPage />} />
+                <Route path="/add-task" element={<TasksFormPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
               </Route>
             </Route>
           </Routes>
         </BrowserRouter>
       </TaskProvider>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
